4d525e1ce20760258c443e5020d7e2b3a9e16c30
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2022-07-21 18:23:12 +0900

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# unwallet-contracts
-unWallet contracts
+# unWallet contracts
---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from "hardhat/types";
+
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.15",
+  networks: {},
+};
+
+export default config;
