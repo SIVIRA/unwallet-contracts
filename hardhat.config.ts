ecfc61f63caed62ca957f4acf2e774b31e40c8cf
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2022-07-25 18:33:38 +0900

    feat: remove SPDX license identifier in preprocessing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,17 @@
 import { HardhatUserConfig } from "hardhat/types";
 
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-preprocessor";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {},
+  preprocess: {
+    eachLine: () => ({
+      transform: (line: string) =>
+        line.startsWith("// SPDX-License-Identifier:") ? "" : line,
+    }),
+  },
 };
 
 export default config;
