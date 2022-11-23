6fbcbd2e153ceb1b5684ed9c597644422a941798
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2022-11-23 18:23:49 +0900

    fix: hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
+import { NetworksUserConfig } from "hardhat/types";
 
 import "@nomicfoundation/hardhat-toolbox";
 
+const networks: NetworksUserConfig = {};
+if (
+  process.env.NETWORK !== undefined &&
+  process.env.RPC_URL !== undefined &&
+  process.env.PRIVATE_KEY !== undefined
+) {
+  networks[process.env.NETWORK] = {
+    url: process.env.RPC_URL,
+    accounts: [process.env.PRIVATE_KEY],
+  };
+}
+
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
-  networks: {},
+  networks: networks,
   gasReporter: {
     enabled: true,
   },
